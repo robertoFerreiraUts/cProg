ef1a772112976bd43705a5c2eeac956890b436d8
Ark442 <alankocharian@gmail.com>
2019-05-28 21:06:41 +1000

    Update Assignment-3.c

--- Assignment-3.c
+++ Assignment-3.c
@@ -25,6 +25,14 @@
  *
  *
 ********************************************************************************/
+#define MAX_QUES_LEN 100
+#define MAX_ANS_LEN 50
+#define MIN_USERNAME_LEN 5
+#define MIN_PASS_LEN 8
+#define MAX_ENTRY 500
+#define MAX_USERINFO_LEN 25
+#define MAX_REC_QUES 3		
+#define INVALID_USERNAME_CHAR "`~!@#$%^&*()-+=[]\\{}|;\':\",./<>? "
 
 /********************************************************************************
  *	Structs
@@ -47,7 +55,7 @@ typedef struct user user_t;
 ********************************************************************************/
 void pre_menu(void);
 user_t login(void);
-void create_user(void);
+user_t create_user(void);
 void change_password(void);
 void reset(void);
 void main_menu(void);
@@ -74,14 +82,14 @@ int main (void)
 }
 
 /********************************************************************************
- *	PasswordEntry
+ *	login
  *
  *
 ********************************************************************************/
 
 user_t login (void)
 {
-	user_t user;
+	user_t user = {0, 0, 0, 0};
 	/* CODE */
 	return user;
 }
@@ -114,7 +122,7 @@ void reset (void)
  *
 ********************************************************************************/
 
-void start_menu (void)
+void pre_menu (void)
 {
 	/* CODE */return;
 }
@@ -131,12 +139,13 @@ void main_menu (void)
 	"1. Encrypt File\n"
 	"2. Decrypt File\n"
 	"3. Search File\n"
-	"4. Exit the program\n"
+	"4. Create User\n"	/* Temporary, exits upon completion */
+	"5. Exit the program\n"
 	"Enter choice (Number between 1-4)>\n");
 }
 
 /********************************************************************************
- *	Encrypt
+ *	main_menu_select
  *
  *
 ********************************************************************************/
@@ -144,7 +153,7 @@ void main_menu (void)
 void main_menu_select (char* filename)
 {
 	int i = 0;
-	while (i != 4)
+	while (i != 5)
 	{
 		main_menu();
 		scanf("%d", &i);
@@ -157,7 +166,9 @@ void main_menu_select (char* filename)
 				break; 
 		case 3: search(filename);
 				break;
-		case 4: exit(0);
+		case 4: create_user();
+				break;
+		case 5: exit(0);
 				break;
 		default: 
 			printf("Invalid choice\n");
@@ -165,6 +176,159 @@ void main_menu_select (char* filename)
 	}
 
 }
+
+/********************************************************************************
+ *	createuser
+ *
+ *
+********************************************************************************/
+
+user_t create_user (void)
+{
+	while ((getchar()) != '\n');
+	char TempName[MAX_ENTRY], TempPass[MAX_ENTRY];
+	char Ques[MAX_REC_QUES + 1][MAX_ENTRY], Ans[MAX_REC_QUES + 1][MAX_ENTRY];
+	int i = 0, l = 0;
+	FILE * Userfile;
+
+	/* Entering "*" canceles user creation */
+	printf("Enter * at any time to cancel.\n");
+
+	/* Username entry */
+	for (i = 0; i < 1; i++)
+	{
+		printf("Enter a username that is between 5 to 25 characters long using only letters, underscores and/or numbers, or enter * to go back\n");
+		fgets(TempName, MAX_ENTRY, stdin);
+
+		/* Checking for cancel */
+		if (strcmp(TempName, "*\n") == 0)
+		{
+			printf("Cancelling.\n");
+			main();
+		}
+
+		/* Checking for previous used usernames */
+		Userfile = fopen(TempName, "r");
+		if (Userfile != NULL)
+		{
+			printf("Your username is the same as another user, please try a different username or add numbers e.g. Username1\n");
+			i--;
+		}
+
+		/* Username valid?, + 1 to account for newline character appended by fgets */
+		if ((strlen(TempName)) > MAX_USERINFO_LEN + 1 || (strlen(TempName)) < MIN_USERNAME_LEN + 1 || (strpbrk(TempName, INVALID_USERNAME_CHAR)) > 0)
+		{
+			printf("Your username is too long, too short or contains invalid characters, please try using a different username.\n");
+			i--;
+		}
+	}
+
+	/* Password Entry */
+	for (i = 0; i < 1; i++)
+	{
+		printf("Enter a password that is between 8 to 25 characters long, enter * to cancel\n");
+		fgets(TempPass, MAX_ENTRY, stdin);
+
+		/* Checking for cancel */
+		if (strcmp(TempPass, "*\n") == 0)
+		{
+			printf("Cancelling.\n");
+			main();
+		}
+
+		/* Password Valid? */
+		if ((strlen(TempPass)) > MAX_USERINFO_LEN + 1 || (strlen(TempPass)) < MIN_PASS_LEN + 1)
+		{
+			printf("Your password is too short or too long, please try a different password.\n");
+			i--;
+		}
+	}
+	
+	/* Recovery questions */
+	for (i = 0; i < 1; i++)
+	{
+		printf("Add recovery questions to your account.\n");
+
+		for (l = 1; l < MAX_REC_QUES + 1; l++)
+		{
+			for (i = 0; i < 1; i++)
+			{
+				/* Entering their question */
+				printf("Enter recovery question %d (Maximum 100 characters)\n", l);
+				fgets(Ques[l], MAX_ENTRY, stdin);
+
+				/* Checking for cancel */
+				if ((strcmp(Ques[l], "*\n")) == 0)
+				{
+					printf("Cancelling.\n");
+					main();
+				}
+
+				/* Valid input? */
+				else if (strlen(Ques[l]) > MAX_QUES_LEN + 1 || strlen(Ques[l]) < 1)
+				{
+					printf("Invalid input, too long or no input.\n");
+					i--;
+				}
+			}
+
+			for (i = 0; i < 1; i++)
+			{
+				/* Entering their answer */
+				printf("Enter the answer to: %s", Ques[l]);
+				fgets(Ans[l], MAX_ENTRY, stdin);
+
+				/* Checking for cancel */
+				if ( (strcmp(Ans[l], "*\n")) == 0)
+				{
+					printf("Cancelling.\n");
+					main();
+				}
+
+				/* Valid input? */
+				else if (strlen(Ans[l]) > MAX_ANS_LEN + 1 || strlen(Ans[l]) < 1)
+				{
+					printf("Invalid input, too long or no input.\n");
+					i--;
+				}
+			}
+		}
+	}
+
+	/* Saving Username & Password */
+	
+	Userfile = fopen(TempName, "w");
+
+	/* File creation successful? */
+    if (Userfile == NULL) {
+        printf("Something went wrong, you may not have permission to create files, cancelling.\n");
+        main();
+    }
+
+    /* Saving username, password */
+	fprintf(Userfile, "Username: %s\nPassword: %s\n", TempName, TempPass);
+
+	/* Saving security questions */
+	for (i = 1; i < MAX_REC_QUES + 1; i++)
+	{
+		fprintf(Userfile, "Q%d: %sA%d: %s\n", i, Ques[i], i, Ans[i]);
+	}
+
+	/* Closing file */
+	fclose(Userfile);
+
+	/* Returning struct */
+
+	user_t user = {TempName, TempPass, 3, 7};
+
+	return user;
+}
+
+/********************************************************************************
+ *	Encrypt
+ *
+ *
+********************************************************************************/
 
 void encrypt (char* filename)
 {
